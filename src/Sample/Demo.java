ee236f3710e3c61d435189900f3598e73a24adc9
KaveriWale <kaveriwale2020@gmail.com>
2025-03-28 17:16:43 +0530

    Added Eclipse Project

---
+++ src/Sample/Demo.java
@@ -0,0 +1,27 @@
+package Sample;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args)
+	{
+		int arr[]= {23,22,45,66,11};
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array");
+		int a1=sc.nextInt();
+		for(int i=0;i<a1;i++)
+		{
+			int max=0;
+			if(i<arr[i])
+			{
+				System.out.println("Enter max");
+			}
+			else
+			{
+				System.out.println("Not max");
+			}
+			
+		}
+
+	}
+
+}
